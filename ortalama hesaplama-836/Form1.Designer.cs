8f4da060434ea0a26bbfe07c9bb66ef7a2e352a1
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-10 16:37:37 +0300

    Add files via upload

---
+++ ortalama hesaplama-836/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ortalama_hesaplama_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(29, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Sınav";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(29, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Sınav";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(-7, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "1. Performans";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(-7, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "2. Performans";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(29, 265);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(136, 25);
+            this.lblDurum.TabIndex = 4;
+            this.lblDurum.Text = "Aldığı belge; ";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(125, 58);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(190, 20);
+            this.txt1.TabIndex = 5;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(125, 94);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(190, 20);
+            this.txt2.TabIndex = 6;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(125, 138);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(190, 20);
+            this.txt3.TabIndex = 7;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(125, 183);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(190, 20);
+            this.txt4.TabIndex = 8;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(391, 54);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(229, 154);
+            this.btnHesapla.TabIndex = 9;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ ortalama hesaplama-836/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ortalama_hesaplama_836
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double sinav1, sinav2, performans1, performans2;
+            double sonuc;
+
+            sinav1 = Convert.ToDouble(txt1.Text);
+            sinav2 = Convert.ToDouble(txt2.Text);
+            performans1= Convert.ToDouble(txt3.Text);
+            performans2= Convert.ToDouble(txt4.Text);
+
+            sonuc = (sinav1 + sinav2 + performans1 + performans2) / 4;
+
+            if (sonuc < 50)
+            {
+                lblDurum.Text = "Aldığı belge; " + "sınıfta kaldı";
+            }
+            else if (sonuc >= 50 && sonuc < 70)
+            {
+                lblDurum.Text = "Aldığı belge; " + "sınıftan geçti";
+            }
+            else if (sonuc>=70 && sonuc < 85)
+            {
+                lblDurum.Text = "Aldığı belge; " + "teşekkür belgesi";
+            }
+            else if (sonuc>=85 && sonuc <= 100)
+            {
+                lblDurum.Text = "Aldığı belge; " + "takdir belgesi";
+            }
+        }
+    }
+}
